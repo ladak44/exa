8a9297a05bfc28523c34900a944705bcc9072cc4
ladak44 <marcin.ladak@gmail.com>
2019-03-18 15:36:04 +0100

    ola

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 
 
 
-# all AZ
+# All AZ
 data "oci_identity_availability_domains" "ads" {
     compartment_id = "${var.tenancy_ocid}"
 }
--- output.tf
+++ output.tf
@@ -2,7 +2,3 @@
 output "show_domains" {
   value = "${data.oci_identity_availability_domains.ads.availability_domains}"
 }
-
-output "test" {
-  value = "xyz"
-}
